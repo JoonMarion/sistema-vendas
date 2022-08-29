c53007fd3ba089beafc4e7f7dc69610cd33cf3c2
Igor Dantas Quintanilha <igor.dantas@eurekalabs.com.br>
2022-08-29 14:36:54 -0300

    fix: set debug as true

--- project/settings.py
+++ project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-o9g(#^2clz*tx3d#_gfr4#-o7a-&rp*z)y1u-ofw7v=cz%-##*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
